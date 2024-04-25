d6a6d252c362112c0fd8cd6c245490b316df5d47
manawara <marcinnawara@int.pl>
2024-04-25 21:34:58 +0200

    Create funcionality "add cart"

--- src/store/CartContext.js
+++ src/store/CartContext.js
@@ -1,25 +1,53 @@
 import React, { createContext, useContext, useReducer } from 'react'
 
-const CartContext = createContext()
+const CartContext = createContext({
+  items: {},
+  addItem: () => {},
+})
 
 const ADD_CART = 'ADD_CART'
 
 const cartReducer = (state, action) => {
   switch (action.type) {
     case ADD_CART:
-      break
+      const item = action.data
+      let updateItems = [...state.items]
+      const findIndex = updateItems.findIndex((el) => el.uid === item.uid)
+      console.log(findIndex)
+      if (findIndex === -1) {
+        item.count = 1
+        updateItems.push(item)
+      } else {
+        const existingItem = state.items[findIndex]
+        const updateItem = {
+          ...existingItem,
+          count: existingItem.count + 1,
+        }
+        updateItems[findIndex] = updateItem
+      }
+
+      return {
+        ...state,
+        items: updateItems,
+      }
 
     default:
       break
   }
+  return state
 }
 
 export const CartContexProvider = ({ children }) => {
   const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] })
 
+  const addItem = (data) => {
+    dispatchCartAction({ type: ADD_CART, data })
+  }
   const cartItem = {
     items: cart.items,
+    addItem,
   }
+  console.log(cart)
   return <CartContext.Provider value={cartItem}>{children}</CartContext.Provider>
 }
 
